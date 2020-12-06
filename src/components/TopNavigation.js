ae36055ceb936bacddb81cc77301b15b6ea40274
samx23 <skalexsong@gmail.com>
2020-12-06 13:05:44 +0700

    change search bar

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -2,17 +2,13 @@ import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
-import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import { InputBase } from "@material-ui/core";
 
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     color: "white",
-  },
-  icons: {
-    marginLeft: ".2em",
   },
   icon: {
     borderRadius: 0,
@@ -26,25 +22,73 @@ const useStyles = makeStyles({
     textAlign: "center",
     marginRight: ".2em",
   },
-});
+  search: {
+    position: "relative",
+    marginLeft: ".2em",
+    marginTop: "1.2em",
+    borderRadius: 999,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 function TopNavigation() {
-  const header = useStyles();
+  const classes = useStyles();
 
   return (
     <Grid
-      className={header.root}
+      className={classes.root}
       container
       justify="space-between"
       alignItems="center"
     >
-      <Grid className={header.icons}>
-        <IconButton className={header.icon}>
+      <Grid className={classes.search}>
+        <div className={classes.searchIcon}>
           <SearchIcon />
-        </IconButton>
+        </div>
+        <InputBase
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ "aria-label": "search" }}
+        />
       </Grid>
-      <Grid className={header.logo}>
-        <Button component={Link} to="/" className={header.icon}>
+      <Grid className={classes.logo}>
+        <Button component={Link} to="/" className={classes.icon}>
           <HomeIcon />
           <h2> Movie DB</h2>
         </Button>

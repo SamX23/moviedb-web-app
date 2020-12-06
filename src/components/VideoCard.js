373845407ff659ccd36e007264f6ffdc1b13f15a
samx23 <skalexsong@gmail.com>
2020-12-06 10:00:07 +0700

    add youtube trailer on click

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,6 +1,8 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
+import movieTrailer from "movie-trailer";
 import Card from "@material-ui/core/Card";
 import TextTruncate from "react-text-truncate";
+import Youtube from "./YoutubeContainer";
 import { makeStyles } from "@material-ui/core/styles";
 import { CardActionArea, CardContent, Typography } from "@material-ui/core";
 import { ThumbUpSharp } from "@material-ui/icons";
@@ -9,6 +11,7 @@ const videoCard = makeStyles({
   root: {
     backgroundColor: "#22283a",
     maxWidth: 400,
+    height: 350,
     margin: "5px",
     borderRadius: "10px",
   },
@@ -28,32 +31,48 @@ const videoCard = makeStyles({
 
 const videoBaseUrl = "https://image.tmdb.org/t/p/original/";
 const VideoCard = forwardRef(({ movie }, ref) => {
+  const [trailerURL, setTrailerURL] = useState("");
   const classes = videoCard();
+
+  const handleClick = (movie) => {
+    movieTrailer(movie.title || movie.original_title || "")
+      .then((url) => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerURL(urlParams.get("v"));
+      })
+      .catch((e) => console.log(e));
+  };
 
   return (
     <Card ref={ref} className={classes.root}>
-      <CardActionArea>
-        <img
-          className={classes.media}
-          src={`${videoBaseUrl}${movie.backdrop_path || movie.poster_path}`}
-          alt={movie.title}
-        />
-        <CardContent className={classes.content}>
-          <Typography variant="h5" component="h3">
-            {movie.title || movie.original_name}
-          </Typography>
-          <TextTruncate
-            line={1}
-            element="p"
-            truncateText=".."
-            text={movie.overview}
-          />
-          <Typography variant="body2" component="p">
-            {movie.media_type && `${movie.media_type} -`}
-            {movie.release_date || movie.first_air_date} - <ThumbUpSharp />{" "}
-            {movie.vote_count}
-          </Typography>
-        </CardContent>
+      <CardActionArea onClick={() => handleClick(movie)}>
+        {trailerURL ? (
+          <Youtube id={trailerURL} />
+        ) : (
+          <>
+            <img
+              className={classes.media}
+              src={`${videoBaseUrl}${movie.backdrop_path || movie.poster_path}`}
+              alt={movie.title}
+            />
+            <CardContent className={classes.content}>
+              <Typography variant="h5" component="h3">
+                {movie.title || movie.original_name}
+              </Typography>
+              <TextTruncate
+                line={1}
+                element="p"
+                truncateText=".."
+                text={movie.overview}
+              />
+              <Typography variant="body2" component="p">
+                {movie.media_type && `${movie.media_type} -`}
+                {movie.release_date || movie.first_air_date} - <ThumbUpSharp />{" "}
+                {movie.vote_count}
+              </Typography>
+            </CardContent>
+          </>
+        )}
       </CardActionArea>
     </Card>
   );
---
+++ src/components/YoutubeContainer.js
@@ -0,0 +1,17 @@
+import YouTube from "react-youtube";
+
+export default function Youtube({ id }) {
+  const opts = {
+    height: "350",
+    width: "400",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const onReady = (event) => {
+    event.target.pauseVideo();
+  };
+
+  return <YouTube videoId={id} opts={opts} onReady={onReady} />;
+}

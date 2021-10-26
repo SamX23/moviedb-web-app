baf2643437e8b87f5fa9edca158240a3d0870ec2
Samx23 <skalexsong@gmail.com>
2021-10-26 21:26:36 +0700

    Update : remove comments

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -12,9 +12,6 @@ const useStyles = makeStyles((theme) => ({
 
 const ScrollTop = ({ children, window }) => {
   const classes = useStyles();
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     target: window,
     disableHysteresis: true,

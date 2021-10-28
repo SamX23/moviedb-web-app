e858ec68c20773d470d794f8b0bae9bdc1b3ff5d
Samx23 <skalexsong@gmail.com>
2021-10-28 22:56:20 +0700

    Update : implement react helmet on HOC

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import Head from "./components/Helmet";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Head
+    title="Movie DB Apps | Sami Kalammallah"
+    desc="A simple movie DB apps created by Sami Kalammallah using React."
+  >
+    <App />
+  </Head>,
+  document.getElementById("root")
+);

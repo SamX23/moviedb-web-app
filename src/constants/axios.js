18865b9821d2365ae8407ef3e583f57db0e76278
Sami Kalammallah <skalexsong@gmail.com>
2023-12-19 08:45:23 +0700

    chore: use proper value

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { URL as baseURL } from "./tmdb";
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
+  baseURL,
 });
 
 export default instance;
